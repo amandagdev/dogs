9e9097e258ca43c81d55ac65504d88bf2ce55047
amandagdev <it.amandag@gmail.com>
2021-02-15 00:44:37 -0300

    tests(button): add tests on snapshot and click button

--- src/components/form/button/tests/test.js
+++ src/components/form/button/tests/test.js
@@ -2,10 +2,19 @@ import React from 'react'
 import { render, screen } from '@testing-library/react'
 
 import Button from '..'
+import userEvent from '@testing-library/user-event'
 
 describe('<Button/>', () => {
   it('should render the button', () => {
-    const { container } = render(<Button />)
+    const { container } = render(<Button>Click!</Button>)
+
     expect(container.firstChild).toMatchSnapshot()
+  })
+
+  it('calls onClick prop when clicked', () => {
+    const handleClick = jest.fn()
+    render(<Button onClick={handleClick}>Click me</Button>)
+    userEvent.click(screen.getByRole('button'))
+    expect(handleClick).toHaveBeenCalledTimes(1)
   })
 })
